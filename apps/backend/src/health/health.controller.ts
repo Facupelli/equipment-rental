dc39b51788adc39a2a9c309c897055b53db61d67
Facundo Personal <facundopellicer4@gmail.com>
2026-03-18 10:13:09 -0300

    make health public

--- apps/backend/src/health/health.controller.ts
+++ apps/backend/src/health/health.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from 'src/modules/auth/infrastructure/is-public.decorator';
 
+@Public()
 @Controller('health')
 export class HealthController {
   @Get()
